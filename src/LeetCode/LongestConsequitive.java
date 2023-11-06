60c86e1599c278438b2b9325e87320e5c7ebbbc5
santosherrera-lopez <santoslopez890@gmail.com>
2023-11-06 18:53:15 -0500

    Find duplicates in an array

--- src/LeetCode/LongestConsequitive.java
+++ src/LeetCode/LongestConsequitive.java
@@ -16,10 +16,7 @@ public class LongestConsequitive {
             for (int sorted:nums) {
                 seen.add(sorted);
             }
-
             for (int i:seen) {
-
-
                 if(i==checker+1 || length==0 ){
                     checker=i;
                     length++;
@@ -33,6 +30,26 @@ public class LongestConsequitive {
             Collections.sort(consecutive);
             return consecutive.get(consecutive.size()-1);
         }
+public int sokfokf(int[] nums) {
+    Set<Integer> set = new HashSet<>();
+    for (int num : nums) {
+        set.add(num);
+    }
+    int maxStreak = 0;
+    for (int num : set) {
+        int currentStreak = 1;
+        if (set.contains(num + 1)) {
+            currentStreak += 1;
+        } else {
+            while (set.contains(num - 1)) {
+                currentStreak += 1;
+                num -= 1;
+            }
+        }
+        maxStreak = Math.max(currentStreak, maxStreak);
+    }
+    return maxStreak;
+}
 
     public static void main(String[] args) {
         int[] test={9,1,4,7,3,-1,0,5,8,-1,6};
