5cc82b93d30fde623dbe5deedde5d6e736d4c340
santosherrera-lopez <santoslopez890@gmail.com>
2024-01-21 23:33:58 -0500

    Added some behavioral questions because I can

---
+++ src/Behavioral/Intro.java
@@ -0,0 +1,11 @@
+package Behavioral;
+
+public class Intro {
+    //Use the Present Past Future Template
+
+    //present- describe your bootcamp and list the skills you learned and mention you won a hackathon recently
+
+    //past - Describe your associates degree in computer science- list the fact you were vp of Network Information and Computer Science Club and say how you came to that
+
+    //furture- look at what the jobs want make them see you working in their company
+}
